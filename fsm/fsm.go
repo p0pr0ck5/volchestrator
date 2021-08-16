c47ec7c5dbe1f27e6bb6c05e29268c84ee00856e
Robert Paprocki <robert@cryptobells.com>
2021-08-16 13:44:05 -0700

    add simple fsm

---
+++ fsm/fsm.go
@@ -0,0 +1,109 @@
+package fsm
+
+import (
+	"fmt"
+	"sync"
+)
+
+type State interface {
+	Value() string
+}
+
+type EventArg interface{}
+
+type Event struct {
+	f *FSM
+
+	PreviousState State
+	NextState     State
+
+	Args []EventArg
+}
+
+type Callback func(e Event) error
+
+type Transition struct {
+	State    State
+	Callback Callback
+}
+
+type TransitionMap map[State][]Transition
+
+type FSM struct {
+	CurrentState State
+	stateMutex   sync.Mutex
+
+	transitionMap      TransitionMap
+	transitionMapMutex sync.Mutex
+
+	transitionHistory []Event
+}
+
+func NewFSM(defaultState State) (*FSM, error) {
+	f := &FSM{
+		CurrentState:  defaultState,
+		transitionMap: make(TransitionMap),
+	}
+
+	return f, nil
+}
+
+func (f *FSM) AddTransitions(state State, transitions []Transition) error {
+	f.transitionMapMutex.Lock()
+	defer f.transitionMapMutex.Unlock()
+
+	f.transitionMap[state] = append(f.transitionMap[state], transitions...)
+
+	return nil
+}
+
+func (f *FSM) Can(newState State) bool {
+	f.stateMutex.Lock()
+	defer f.stateMutex.Unlock()
+
+	for _, candidate := range f.transitionMap[f.CurrentState] {
+		if candidate.State == newState {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (f *FSM) Transition(newState State, args ...EventArg) error {
+	if !f.Can(newState) {
+		return fmt.Errorf("invalid state transition %q", newState)
+	}
+
+	f.stateMutex.Lock()
+	defer f.stateMutex.Unlock()
+
+	var transition Transition
+	for _, candidate := range f.transitionMap[f.CurrentState] {
+		if candidate.State == newState {
+			transition = candidate
+		}
+	}
+
+	e := Event{
+		f:             f,
+		PreviousState: f.CurrentState,
+		NextState:     transition.State,
+		Args:          args,
+	}
+
+	f.CurrentState = newState
+	f.transitionHistory = append(f.transitionHistory, e)
+
+	if cb := transition.Callback; cb != nil {
+		if err := cb(e); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (f *FSM) Value() string {
+	return f.CurrentState.Value()
+}
---
+++ fsm/fsm_test.go
@@ -0,0 +1,410 @@
+package fsm
+
+import (
+	"errors"
+	"testing"
+)
+
+type mockState string
+
+func (m mockState) Value() string {
+	return string(m)
+}
+
+const fooState = mockState("foo")
+const barState = mockState("bar")
+const bazState = mockState("baz")
+const batState = mockState("bat")
+
+func TestNewFSM(t *testing.T) {
+	type args struct {
+		defaultState State
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			"new fsm",
+			args{
+				defaultState: fooState,
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := NewFSM(tt.args.defaultState)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewFSM() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
+
+func TestFSM_Value(t *testing.T) {
+	type fields struct {
+		CurrentState State
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			"foo",
+			fields{
+				CurrentState: fooState,
+			},
+			"foo",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := &FSM{
+				CurrentState: tt.fields.CurrentState,
+			}
+			if got := f.Value(); got != tt.want {
+				t.Errorf("FSM.Value() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFSM_Can(t *testing.T) {
+	type fields struct {
+		CurrentState  State
+		transitionMap TransitionMap
+	}
+	type args struct {
+		newState State
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   bool
+	}{
+		{
+			"can transition - single candidate",
+			fields{
+				CurrentState: fooState,
+				transitionMap: TransitionMap{
+					fooState: []Transition{
+						{
+							State: barState,
+						},
+					},
+				},
+			},
+			args{
+				newState: barState,
+			},
+			true,
+		},
+		{
+			"can transition - multiple candidates (first)",
+			fields{
+				CurrentState: fooState,
+				transitionMap: TransitionMap{
+					fooState: []Transition{
+						{
+							State: barState,
+						},
+						{
+							State: bazState,
+						},
+					},
+				},
+			},
+			args{
+				newState: barState,
+			},
+			true,
+		},
+		{
+			"can transition - multiple candidates (non-first)",
+			fields{
+				CurrentState: fooState,
+				transitionMap: TransitionMap{
+					fooState: []Transition{
+						{
+							State: barState,
+						},
+						{
+							State: bazState,
+						},
+					},
+				},
+			},
+			args{
+				newState: bazState,
+			},
+			true,
+		},
+		{
+			"cannot transition - single candidate",
+			fields{
+				CurrentState: fooState,
+				transitionMap: TransitionMap{
+					fooState: []Transition{
+						{
+							State: barState,
+						},
+					},
+				},
+			},
+			args{
+				newState: bazState,
+			},
+			false,
+		},
+		{
+			"cannot transition - multiple candidates",
+			fields{
+				CurrentState: fooState,
+				transitionMap: TransitionMap{
+					fooState: []Transition{
+						{
+							State: barState,
+						},
+						{
+							State: fooState,
+						},
+					},
+				},
+			},
+			args{
+				newState: bazState,
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := &FSM{
+				CurrentState:  tt.fields.CurrentState,
+				transitionMap: tt.fields.transitionMap,
+			}
+			if got := f.Can(tt.args.newState); got != tt.want {
+				t.Errorf("FSM.Can() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFSM_Transition(t *testing.T) {
+	type fields struct {
+		CurrentState  State
+		transitionMap TransitionMap
+	}
+	type args struct {
+		newState State
+		args     []EventArg
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    State
+		wantErr bool
+	}{
+		{
+			"simple transition",
+			fields{
+				CurrentState: fooState,
+				transitionMap: TransitionMap{
+					fooState: []Transition{
+						{
+							State: barState,
+						},
+					},
+				},
+			},
+			args{
+				newState: barState,
+			},
+			barState,
+			false,
+		},
+		{
+			"invalid transition",
+			fields{
+				CurrentState: fooState,
+				transitionMap: TransitionMap{
+					fooState: []Transition{
+						{
+							State: barState,
+						},
+					},
+				},
+			},
+			args{
+				newState: bazState,
+			},
+			fooState,
+			true,
+		},
+		{
+			"transition with callback",
+			fields{
+				CurrentState: fooState,
+				transitionMap: TransitionMap{
+					fooState: []Transition{
+						{
+							State:    barState,
+							Callback: func(e Event) error { return nil },
+						},
+					},
+				},
+			},
+			args{
+				newState: barState,
+			},
+			barState,
+			false,
+		},
+		{
+			"transition with erroring callback",
+			fields{
+				CurrentState: fooState,
+				transitionMap: TransitionMap{
+					fooState: []Transition{
+						{
+							State:    barState,
+							Callback: func(e Event) error { return errors.New("e") },
+						},
+					},
+				},
+			},
+			args{
+				newState: barState,
+			},
+			barState,
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := &FSM{
+				CurrentState:  tt.fields.CurrentState,
+				transitionMap: tt.fields.transitionMap,
+			}
+			if err := f.Transition(tt.args.newState, tt.args.args...); (err != nil) != tt.wantErr {
+				t.Errorf("FSM.Transition() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if current := f.CurrentState; current != tt.want {
+				t.Errorf("FSM.CurrentState = %v, want %v", current, tt.want)
+			}
+		})
+	}
+}
+
+func TestFSM_AddTransitions(t *testing.T) {
+	type fields struct {
+		CurrentState  State
+		transitionMap TransitionMap
+	}
+	type args struct {
+		state       State
+		transitions []Transition
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			"add single initial transition",
+			fields{
+				transitionMap: make(TransitionMap),
+			},
+			args{
+				state: fooState,
+				transitions: []Transition{
+					{
+						State: barState,
+					},
+				},
+			},
+			false,
+		},
+		{
+			"add multiple initial transition",
+			fields{
+				transitionMap: make(TransitionMap),
+			},
+			args{
+				state: fooState,
+				transitions: []Transition{
+					{
+						State: barState,
+					},
+					{
+						State: bazState,
+					},
+				},
+			},
+			false,
+		},
+		{
+			"add single transition to existing transitions",
+			fields{
+				transitionMap: TransitionMap{
+					fooState: []Transition{
+						{
+							State: barState,
+						},
+					},
+				},
+			},
+			args{
+				state: fooState,
+				transitions: []Transition{
+					{
+						State: bazState,
+					},
+					{
+						State: batState,
+					},
+				},
+			},
+			false,
+		},
+		{
+			"add multiple transitions to existing transitions",
+			fields{
+				transitionMap: TransitionMap{
+					fooState: []Transition{
+						{
+							State: barState,
+						},
+					},
+				},
+			},
+			args{
+				state: fooState,
+				transitions: []Transition{
+					{
+						State: bazState,
+					},
+				},
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := &FSM{
+				CurrentState:  tt.fields.CurrentState,
+				transitionMap: tt.fields.transitionMap,
+			}
+			if err := f.AddTransitions(tt.args.state, tt.args.transitions); (err != nil) != tt.wantErr {
+				t.Errorf("FSM.AddTransitions() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
